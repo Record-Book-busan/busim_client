9e477f623d456841ee95323b69df87d9db27fd4e
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-20 17:04:26 +0900

    refactor: 모든 파일 linting

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	presets: ['module:@react-native/babel-preset'],
-	plugins: [
-		"nativewind/babel",
-		[
-			'module-resolver',
-			{
-				root: ['./src'],
-				extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-				alias: {
-					'@': './src',
-				},
-			},
-		],
-		'inline-dotenv',
-		'react-native-reanimated/plugin',
-	],
-};
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    'inline-dotenv',
+    'react-native-reanimated/plugin',
+  ],
+}
