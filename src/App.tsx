e4b71bf25e38dc531f1df65d2ff58c6800527261
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-26 12:28:30 +0900

    style: liting 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import "react-native-gesture-handler";
+import 'react-native-gesture-handler'
 
-import { useEffect } from "react";
-import { initializeKakaoSDK } from "@react-native-kakao/core";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MMKV } from "react-native-mmkv";
+import { useEffect } from 'react'
+import { initializeKakaoSDK } from '@react-native-kakao/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { MMKV } from 'react-native-mmkv'
 
-import { ThemeProvider } from "@/theme";
+import ApplicationNavigator from './navigators/Application'
+import './translations'
 
-import ApplicationNavigator from "./navigators/Application";
-import "./translations";
+export const queryClient = new QueryClient()
 
-export const queryClient = new QueryClient();
-
-export const storage = new MMKV();
+export const storage = new MMKV()
 
 async function initializeSDK() {
   try {
@@ -21,24 +19,22 @@ async function initializeSDK() {
         javascriptKey: `${process.env.KakaoJsApiKey}`,
         restApiKey: `${process.env.KakaoRestApiKey}`,
       },
-    });
+    })
   } catch (error) {
-    console.error("kakao SDK 초기화 중 오류 발생", error);
+    console.error('kakao SDK 초기화 중 오류 발생', error)
   }
 }
 
 function App() {
   useEffect(() => {
-    void initializeSDK();
-  }, []);
+    void initializeSDK()
+  }, [])
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider storage={storage}>
-        <ApplicationNavigator />
-      </ThemeProvider>
+      <ApplicationNavigator />
     </QueryClientProvider>
-  );
+  )
 }
 
-export default App;
+export default App
