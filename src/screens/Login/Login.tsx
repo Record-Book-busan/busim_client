b17558db899d58f20b331f62d31584a9d1778094
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-30 15:00:14 +0900

    fix: 소셜 로그인 가이드라인에 맞게 수정

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,14 +1,15 @@
 import { login, isLogined } from '@react-native-kakao/user'
-import { useNavigation } from '@react-navigation/native'
 import { useState, useCallback } from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 
-import { logoWhite } from '@/assets'
-import { SafeScreen, SvgIcon, ImageVariant } from '@/shared'
+import icoLogo from '@/assets/images/logo_white.png'
+import { ImageVariant, SafeScreen, SvgIcon } from '@/shared'
+import icoNotice from '@/theme/assets/images/ico-notice.png'
+import icoUser from '@/theme/assets/images/ico-user.png'
+import { RootScreenProps } from '@/types/navigation'
 
-function Login() {
+function Login({ navigation }: RootScreenProps<'Login'>) {
   const [notice, setNotice] = useState('')
-  const navigation = useNavigation()
 
   /**
    * 카카오 로그인
@@ -45,41 +46,42 @@ function Login() {
 
   return (
     <SafeScreen>
-      <View className="flex w-full flex-1 items-center rounded-xl bg-white">
+      <View className="flex flex-1 items-center bg-white">
         {!!notice && (
-          <View className="absolute top-12 w-3/4 flex-row items-center rounded-xl border border-[#FF0000] bg-[#FFF0F0] px-3 py-4">
-            <SvgIcon name="notice" />
-            <Text className="ml-2 text-sm font-semibold text-black">{notice}</Text>
+          <View className="absolute top-12 w-3/4 flex-row items-center border border-[#FF0000] bg-[#FFF0F0] px-3 py-4">
+            <ImageVariant source={icoNotice} />
+            <Text className="ml-2 font-semibold">{notice}</Text>
           </View>
         )}
-        <ImageVariant className="mt-52 h-32 w-3/4" source={logoWhite} />
-        <Text className="text-sm text-black">안녕하세요!</Text>
-        <Text className="mb text-sm text-black">끼록부에 오신 것을 환영합니다.</Text>
-        <Text className="my-12 text-xl font-bold text-black">로그인하기</Text>
-        <View className="w-3/4 gap-2 rounded-xl">
-          <TouchableOpacity className="rounded-xl bg-yellow-300" onPress={wrapKakaoLogin}>
-            <View className="absolute left-5 top-1/2 -translate-y-2">
-              <SvgIcon name="kakao" />
-            </View>
-            <Text className="py-4 text-center text-sm text-black">카카오로 계속하기</Text>
+        <ImageVariant className="mt-52 h-32 w-3/4 bg-red-200" source={icoLogo} />
+        <Text>안녕하세요!</Text>
+        <Text className="mb">끼록부에 오신 것을 환영합니다.</Text>
+        <Text className="my-12 text-xl font-bold">로그인하기</Text>
+        <View className="w-3/4 gap-2">
+          <TouchableOpacity
+            className="flex-row items-center justify-center rounded-lg bg-[#FEE500] py-4"
+            onPress={wrapKakaoLogin}
+          >
+            <SvgIcon name="kakao" className="mr-2" size={20} />
+            <Text className="text-[#191919]">카카오 로그인</Text>
           </TouchableOpacity>
-          <TouchableOpacity className="rounded-xl bg-black">
-            <View className="absolute left-5 top-1/2 -translate-y-4">
-              <SvgIcon name="apple" />
-            </View>
-            <Text className="py-4 text-center text-sm text-white">Apple로 계속하기</Text>
+
+          <TouchableOpacity className="flex-row items-center justify-center rounded-lg bg-black py-4">
+            <SvgIcon name="apple" className="mr-2" size={20} />
+            <Text className="text-white">Apple로 로그인</Text>
           </TouchableOpacity>
-          <TouchableOpacity className="rounded-xl border border-gray-300 bg-white">
-            <View className="absolute left-5 top-1/2 -translate-y-3">
-              <SvgIcon name="google" />
-            </View>
-            <Text className="py-4 text-center text-sm text-black">Google로 계속하기</Text>
+
+          <TouchableOpacity className="flex-row items-center justify-center rounded-lg border border-gray-300 bg-white py-4">
+            <SvgIcon name="google" className="mr-2" size={20} />
+            <Text className="font-[Roboto-Medium] text-[#757575]">Google로 로그인</Text>
           </TouchableOpacity>
-          <TouchableOpacity className="rounded-xl bg-gray-300" onPress={unAuthorizedLogin}>
-            <View className="absolute left-5 top-1/2 -translate-y-3">
-              <SvgIcon name="user" />
-            </View>
-            <Text className="py-4 text-center text-sm text-black">비회원으로 계속하기</Text>
+
+          <TouchableOpacity
+            className="flex-row items-center justify-center rounded-lg bg-gray-300 py-4"
+            onPress={unAuthorizedLogin}
+          >
+            <ImageVariant className="mr-2" source={icoUser} />
+            <Text className="text-[#191919]">비회원으로 계속하기</Text>
           </TouchableOpacity>
         </View>
       </View>
