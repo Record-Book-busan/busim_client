6a48a878fcedece71029ec71cc661b1619b13dd8
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-30 14:59:57 +0900

    refactor: 타입 변경 in RootStackParamList

---
+++ src/types/navigation.ts
@@ -0,0 +1,33 @@
+import type { NavigatorScreenParams } from '@react-navigation/native'
+import type { StackScreenProps } from '@react-navigation/stack'
+
+export type RootStackParamList = {
+  Login: undefined
+  MainTab: undefined
+  Record: NavigatorScreenParams<RecordStackParamList>
+  MyPage: undefined
+}
+
+export type MainTabParamList = {
+  Map: undefined
+  Camera: undefined
+  MyPage: undefined
+}
+
+export type MapStackParamList = {
+  MapHome: undefined
+  MapRecommend: undefined
+}
+
+export type RecordStackParamList = {
+  CameraCapture: undefined
+  CreatePost: undefined
+}
+
+export type MyPageStackParamList = {
+  MyPageProfile: undefined
+  MyPageSettings: undefined
+}
+
+export type RootScreenProps<S extends keyof RootStackParamList = keyof RootStackParamList> =
+  StackScreenProps<RootStackParamList, S>
