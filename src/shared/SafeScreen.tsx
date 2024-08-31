66e90f94634af99a29d5d89bbe840a60dea0f0d9
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-01 06:19:39 +0900

    refactor: save-area insets 대신 SafeAreaView 사용

--- src/shared/SafeScreen.tsx
+++ src/shared/SafeScreen.tsx
@@ -1,26 +1,21 @@
 import { useColorScheme } from 'nativewind'
-import React from 'react'
-import { StatusBar, View } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { StatusBar } from 'react-native'
+import { SafeAreaView, type Edge } from 'react-native-safe-area-context'
 
 type SafeScreenProps = {
   children: React.ReactNode
-  excludeEdges?: ('top' | 'bottom' | 'left' | 'right')[]
+  excludeEdges?: Edge[]
 }
 
 function SafeScreen({ children, excludeEdges = [] }: SafeScreenProps) {
-  const insets = useSafeAreaInsets()
   const { colorScheme } = useColorScheme()
 
   return (
-    <View
-      className="flex-1 bg-white dark:bg-gray-900"
-      style={{
-        paddingTop: excludeEdges.includes('top') ? 0 : insets.top,
-        paddingBottom: excludeEdges.includes('bottom') ? 0 : insets.bottom,
-        paddingLeft: excludeEdges.includes('left') ? 0 : insets.left,
-        paddingRight: excludeEdges.includes('right') ? 0 : insets.right,
-      }}
+    <SafeAreaView
+      edges={['top', 'right', 'bottom', 'left'].filter(
+        (edge): edge is Edge => !excludeEdges.includes(edge as Edge),
+      )}
+      style={{ flex: 1 }}
     >
       <StatusBar
         barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'}
@@ -28,7 +23,7 @@ function SafeScreen({ children, excludeEdges = [] }: SafeScreenProps) {
         translucent
       />
       {children}
-    </View>
+    </SafeAreaView>
   )
 }
 
