46c8a5a4aa893afcd76019450fb00748ba3d72ba
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-02 15:44:02 +0900

    refactor: Search Header 햄버거 제거

--- src/shared/Header/SearchHeader.tsx
+++ src/shared/Header/SearchHeader.tsx
@@ -1,35 +1,58 @@
-import { View, TextInput, TouchableOpacity, Platform } from 'react-native'
+import { cva, type VariantProps } from 'class-variance-authority'
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  type TouchableOpacityProps,
+} from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-import SvgIcon from '../SvgIcon'
+import { theme } from '@/theme'
+import { cn } from '@/utils/cn'
 
-const HEADER_CONTENT_HEIGHT = 50
-const HEADER_HEIGHT =
-  Platform.OS === 'ios' ? HEADER_CONTENT_HEIGHT + 44 : HEADER_CONTENT_HEIGHT + 10
-// iOS: 상태바(44) + content 높이
-// Android: 상태바(24 - 28) + 액션바(56) TODO: aos에서 확인해보기
+import { SvgIcon } from '../SvgIcon'
 
-function SearchHeader() {
+const HeaderVariants = cva('', {
+  variants: {
+    type: {
+      map: `absolute left-0 right-0 top-0 z-50`,
+      default: '',
+    },
+  },
+  defaultVariants: {
+    type: 'default',
+  },
+})
+
+interface SearchHeaderProps extends VariantProps<typeof HeaderVariants>, TouchableOpacityProps {
+  type?: 'default' | 'map'
+  placeholder?: string
+  containerStyle?: string
+}
+
+export function SearchHeader({ type, placeholder, containerStyle, ...props }: SearchHeaderProps) {
   const insets = useSafeAreaInsets()
 
   return (
     <View
-      className="absolute left-0 right-0 top-0 z-50"
+      className={cn(HeaderVariants({ type }))}
       style={{
         paddingTop: insets.top,
-        height: HEADER_HEIGHT + insets.top,
       }}
     >
-      <View className="flex-row items-center px-4" style={{ height: HEADER_CONTENT_HEIGHT }}>
-        <TouchableOpacity className="mr-2" onPress={() => console.log('show menu.')}>
-          <SvgIcon name="hamburger" className="text-gray-400" />
-        </TouchableOpacity>
-        <View className="flex-1 flex-row items-center rounded-full bg-white px-4 py-2 shadow-md">
+      <View className={cn('flex-row items-center px-4 pb-4 pt-2', containerStyle)}>
+        <View className="flex-1 flex-row items-center rounded-full bg-white px-4 shadow">
           <TextInput
-            className="h-10 flex-1 text-base focus:border-gray-900"
-            placeholder="장소, 버스, 지하철, 주소 검색"
+            className={cn(
+              'h-10 flex-1 items-center text-base',
+              Platform.OS === 'ios' && 'leading-[0px]',
+            )}
+            placeholder={placeholder}
+            placeholderTextColor={theme.colors['BUSIM-gray-light']}
+            textAlignVertical="center"
           />
-          <TouchableOpacity className="ml-2" onPress={() => console.log('search.')}>
+          <TouchableOpacity className="ml-2" onPress={props.onPress} {...props}>
             <SvgIcon name="search" />
           </TouchableOpacity>
         </View>
@@ -37,7 +60,3 @@ function SearchHeader() {
     </View>
   )
 }
-
-export default SearchHeader
-
-export const TOTAL_HEADER_HEIGHT = HEADER_HEIGHT
