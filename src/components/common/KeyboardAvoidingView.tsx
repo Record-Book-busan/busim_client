71c1e6feef0d6ccc3c4b30b1f2d33c2c8a33b0dc
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-03 16:31:17 +0900

    refactor: KeyboardAvoidingView edge 추가

--- src/components/common/KeyboardAvoidingView.tsx
+++ src/components/common/KeyboardAvoidingView.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import { Dimensions, Platform } from 'react-native'
 import Animated, { useAnimatedKeyboard, useAnimatedStyle } from 'react-native-reanimated'
-import { useSafeAreaFrame, useSafeAreaInsets } from 'react-native-safe-area-context'
+import { type Edge, useSafeAreaFrame, useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const height = Dimensions.get('window').height
 
-// TODO: SafeScreen에 따라서 maxHeight 계산 조정이 필요함. 확인해볼것..
-export function KeyboardAvoidingView({ children }: { children: React.ReactNode }) {
+// TODO: 안드로이드 테스트가 필요함
+export function KeyboardAvoidingView({
+  edge,
+  children,
+}: {
+  edge?: Extract<Edge, 'top' | 'bottom'>
+  children: React.ReactNode
+}) {
   const keyboard = useAnimatedKeyboard()
-  const { top } = useSafeAreaInsets()
+  const insets = useSafeAreaInsets()
   const { y } = useSafeAreaFrame()
   const bottomSafeAreaHeight = Platform.OS === 'ios' ? y : 0
 
   const animatedStyles = useAnimatedStyle(() => ({
-    maxHeight: height - keyboard.height.value - bottomSafeAreaHeight - top,
+    maxHeight:
+      height -
+      keyboard.height.value -
+      bottomSafeAreaHeight -
+      (edge?.includes('top') ? insets.top : 0),
   }))
 
   return (
