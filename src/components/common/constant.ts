b3f5821ff5a7d566b3901cfbfac55f56f87d1344
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-04 06:39:19 +0900

    feat: tw 컬러 토큰 추가

---
+++ src/components/common/constant.ts
@@ -0,0 +1,12 @@
+export const CATEGORY = {
+  관광지: 1,
+  자연: 8,
+  테마: 2,
+  레포츠: 16,
+  핫플: 4,
+  맛집: 32,
+  카페: 64,
+  술집: 128,
+} as const
+export type CategoryKey = keyof typeof CATEGORY
+export type CategoryType = (typeof CATEGORY)[keyof typeof CATEGORY]
