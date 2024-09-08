e9312b18f116e0a4217e157d3072189710d1f0a9
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-08 15:20:01 +0900

    refactor: SearchBar, SearchBarView 분리

---
+++ src/components/common/SearchBarView.tsx
@@ -0,0 +1,44 @@
+import { View, TextInput, Platform, Pressable, type PressableProps } from 'react-native'
+
+import { theme } from '@/theme'
+import { cn } from '@/utils/cn'
+
+import { SvgIcon } from '../../shared/SvgIcon'
+
+interface SearchBarProps extends PressableProps {
+  placeholder?: string
+  containerStyle?: string
+}
+
+/**
+ * SearchBar 형태로 생긴 View 컴포넌트.
+ * 별다른 기능은 없습니다.
+ */
+export function SearchBarView({ placeholder, containerStyle, ...props }: SearchBarProps) {
+  return (
+    <View className="left-0 right-0 top-0 z-50">
+      <Pressable
+        className={cn('flex-row items-center px-4 pb-4 pt-2', containerStyle)}
+        onPress={props.onPress}
+        {...props}
+      >
+        <View className="flex-1 flex-row items-center rounded-full bg-white px-4 shadow">
+          <TextInput
+            className={cn(
+              'h-10 flex-1 items-center text-base',
+              Platform.OS === 'ios' && 'leading-[0px]',
+            )}
+            placeholder={placeholder}
+            placeholderTextColor={theme.colors['BUSIM-gray-light']}
+            textAlignVertical="center"
+            editable={false}
+            pointerEvents="none"
+          />
+          <Pressable className="ml-2" pointerEvents="none">
+            <SvgIcon name="search" className="text-BUSIM-blue" />
+          </Pressable>
+        </View>
+      </Pressable>
+    </View>
+  )
+}
