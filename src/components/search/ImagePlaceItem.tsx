4c0c46399dd94fb03b8fbee02a9479d08c235c2e
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-10 03:18:28 +0900

    refactor: search 데이터 구조에 맞게 props 변경

--- src/components/search/ImagePlaceItem.tsx
+++ src/components/search/ImagePlaceItem.tsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import { Text, TouchableOpacity, View } from 'react-native'
 
 import { ImageVariant, SvgIcon } from '@/shared'
+import { ButtonPrimitive } from '@/shared/Button'
 
 import { BookmarkButton } from '../common'
 
@@ -8,50 +10,65 @@ type ImagePlaceItemProps = {
   id: number
   title: string
   category: string
-  description: string
+  address?: string
   onPressBookMark: (id: number) => void
   onPressMove: (id: number) => void
   isBookMarked: boolean
-  imageUrl: string
+  imageUrl?: string
 }
 
 export function ImagePlaceItem({
   id,
   title,
   category,
-  description,
+  address,
   onPressBookMark,
   onPressMove,
   isBookMarked,
-  imageUrl,
+  // imageUrl,
 }: ImagePlaceItemProps) {
   return (
-    <View className="flex flex-row border border-y-[#DBDCE5] px-2 py-4">
-      <View className="flex flex-row">
-        <ImageVariant className="h-24 w-20" source={{ uri: imageUrl }} />
-        <View className="left-[-75px] top-1">
-          <BookmarkButton isBookMarked={isBookMarked} onPress={() => onPressBookMark(id)} />
+    <View className="border-b border-neutral-200 py-3 last:border-b-0">
+      <ButtonPrimitive animationConfig={{ toValue: 0.99 }} onPress={() => onPressMove(id)}>
+        <View className="flex-row">
+          <View className="mr-3">
+            <ImageVariant
+              className="aspect-3/4 h-24 w-20 rounded-lg"
+              source={{ uri: 'https://via.placeholder.com/640x480' }} // FIXME: 디폴트 이미지 추가 필요!
+            />
+            <View className="absolute left-1 top-1">
+              <BookmarkButton
+                size={24}
+                isBookMarked={isBookMarked}
+                onPress={() => onPressBookMark(id)}
+              />
+            </View>
+          </View>
+          <View className="mt-2 flex-1 justify-start">
+            <Text className="text-base font-semibold text-gray-800">{title}</Text>
+            <View className="mt-2 flex-row items-center">
+              <SvgIcon name="category" size={14} />
+              <Text className="ml-1 text-sm text-gray-500">{category}</Text>
+            </View>
+            {address && (
+              <View className="mt-1 flex-row items-center">
+                <SvgIcon name="marker" className="text-gray-400" size={14} />
+                <Text className="ml-1 text-sm text-gray-500" numberOfLines={1}>
+                  {address}
+                </Text>
+              </View>
+            )}
+          </View>
+          <View className="items-center justify-center">
+            <TouchableOpacity
+              className="ml-2 h-7 w-7 items-center justify-center rounded-full bg-BUSIM-blue-dark"
+              onPress={() => onPressMove(id)}
+            >
+              <SvgIcon name="arrowRightWhite" size={20} />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-      <View className="flex flex-auto">
-        <Text>{title}</Text>
-        <View className="flex flex-row items-center">
-          <SvgIcon name="category" />
-          <Text className="ml-1">{category}</Text>
-        </View>
-        <View className="flex flex-row items-center">
-          <SvgIcon name="explain" />
-          <Text className="ml-1">{description}</Text>
-        </View>
-      </View>
-      <View className="flex w-8 justify-center">
-        <TouchableOpacity
-          className="flex h-8 items-center justify-center rounded-full bg-[#2653B0]"
-          onPress={() => onPressMove(id)}
-        >
-          <SvgIcon name="arrowRightWhite" />
-        </TouchableOpacity>
-      </View>
+      </ButtonPrimitive>
     </View>
   )
 }
