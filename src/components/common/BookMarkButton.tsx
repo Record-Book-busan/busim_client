435b0adc08679ca52e8ef7975dc758b5e49458b8
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-10 03:26:34 +0900

    fix: bookmarkButton ui 수정

--- src/components/common/BookMarkButton.tsx
+++ src/components/common/BookMarkButton.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import { TouchableOpacity } from 'react-native'
 
 import { SvgIcon } from '@/shared/SvgIcon'
+import { theme } from '@/theme'
 
 interface Props {
+  size?: number
   isBookMarked: boolean
   onPress: () => void
 }
 
-export const BookmarkButton = ({ isBookMarked, onPress }: Props) => {
+export const BookmarkButton = ({ isBookMarked, size, onPress }: Props) => {
   const [isPressed, setIsPressed] = useState(isBookMarked)
 
   const handlePress = () => {
@@ -20,7 +22,9 @@ export const BookmarkButton = ({ isBookMarked, onPress }: Props) => {
     <TouchableOpacity onPress={handlePress}>
       <SvgIcon
         name="bookmark"
-        className={`h-6 w-6 ${isPressed ? 'text-BUSIM-blue' : 'text-white'}`}
+        size={size}
+        stroke={isPressed ? theme.colors['BUSIM-blue'] : 'white'}
+        className={`h-6 w-6 ${isPressed ? 'text-BUSIM-blue' : 'text-transparent'}`}
       />
     </TouchableOpacity>
   )
