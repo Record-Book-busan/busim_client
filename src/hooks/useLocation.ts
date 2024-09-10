50c2a28c6c689ae7fed7745bb447ef41d38d7e2d
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-10 20:13:43 +0900

    feat: 위치 권한 확인/요청 훅, 현재 위치 정보 가져오는 훅 구현

---
+++ src/hooks/useLocation.ts
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+import Geolocation from 'react-native-geolocation-service'
+
+import { useLocationPermission } from './useLocationPermission'
+
+type Location = {
+  lat: number
+  lng: number
+}
+
+export const useLocation = () => {
+  const [location, setLocation] = useState<Location>({ lng: 126.570667, lat: 33.450701 })
+  const { permissionStatus, requestLocationAccess } = useLocationPermission()
+
+  const verifyLocation = (lng: number, lat: number) => {
+    // 부산의 대략적인 좌표 범위
+    const minLng = 128.7 // 부산 서쪽 경계
+    const maxLng = 129.3 // 부산 동쪽 경계
+    const minLat = 34.9 // 부산 남쪽 경계
+    const maxLat = 35.4 // 부산 북쪽 경계
+
+    if (lng < minLng || lng > maxLng) return false
+    if (lat < minLat || lat > maxLat) return false
+
+    return true
+  }
+
+  const getCurrentLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log('기기 위치:', position.coords.longitude, position.coords.latitude)
+        if (verifyLocation(position.coords.longitude, position.coords.latitude)) {
+          console.log('통과!')
+          setLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          })
+        }
+      },
+      error => {
+        console.log(error.code, error.message)
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+    )
+  }
+
+  useEffect(() => {
+    if (permissionStatus === 'granted') {
+      getCurrentLocation()
+    }
+  }, [permissionStatus])
+
+  const refreshLocation = async () => {
+    const status = await requestLocationAccess()
+    if (status === 'granted') {
+      getCurrentLocation()
+    }
+  }
+
+  return { location, refreshLocation }
+}
---
+++ src/hooks/useLocationPermission.ts
@@ -0,0 +1,62 @@
+import { useEffect, useState, useCallback } from 'react'
+import { Alert, Platform } from 'react-native'
+import {
+  check,
+  request,
+  PERMISSIONS,
+  RESULTS,
+  openSettings,
+  type PermissionStatus,
+} from 'react-native-permissions'
+
+const LOCATION_PERMISSION = Platform.select({
+  ios: PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
+  android: PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+})
+
+export const useLocationPermission = () => {
+  const [permissionStatus, setPermissionStatus] = useState<PermissionStatus | null>(null)
+
+  const checkPermission = async () => {
+    if (!LOCATION_PERMISSION) return null
+
+    const result = await check(LOCATION_PERMISSION)
+    setPermissionStatus(result)
+
+    return result
+  }
+
+  const requestPermission = async () => {
+    if (!LOCATION_PERMISSION) return null
+
+    const result = await request(LOCATION_PERMISSION)
+    setPermissionStatus(result)
+
+    return result
+  }
+
+  const requestLocationAccess = useCallback(async () => {
+    const status = await checkPermission()
+    // console.log('Initial permission status:', status)
+
+    if (status === RESULTS.DENIED) {
+      return await requestPermission()
+    } else if (status === RESULTS.BLOCKED) {
+      Alert.prompt('위치 권한이 차단되어 있습니다.') // TODO: 알럿모달 UI 띄우기
+      await openSettings()
+    }
+
+    // console.log('위치 권한 상태:', status)
+    return status
+  }, [checkPermission, requestPermission])
+
+  useEffect(() => {
+    void checkPermission()
+  }, [checkPermission])
+
+  return {
+    permissionStatus,
+    requestLocationAccess,
+    checkPermission,
+  }
+}
