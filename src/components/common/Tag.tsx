1478a0078eb2b4cd0612e1e61fbfb246da0ad396
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-13 07:58:16 +0900

    fix: 카테고리 서버 인터페이스에 맞게 변경

--- src/components/common/Tag.tsx
+++ src/components/common/Tag.tsx
@@ -3,57 +3,55 @@ import { Text, View } from 'react-native'
 
 import { cn } from '@/utils/cn'
 
-import { CATEGORY, type CategoryKey } from '../../constants/data'
+import { CATEGORY, CategoryType, type CategoryKey } from '../../constants/data'
 
 const tagVariants = cva('rounded-full min-w-[60px] px-5 py-2', {
   variants: {
     category: {
-      관광지: 'bg-BUSIM-blue',
-      자연: 'bg-BUSIM-summber-green',
-      테마: 'bg-BUSIM-deep-lavender',
-      레포츠: 'bg-BUSIM-ball-blue',
-      핫플: 'bg-BUSIM-cherry',
-      맛집: 'bg-BUSIM-beaver',
-      특별한_맛집: 'bg-BUSIM-thulian-pink',
-      술집: 'bg-BUSIM-warm-pink',
+      [CATEGORY.관광지]: 'bg-BUSIM-blue',
+      [CATEGORY.자연]: 'bg-BUSIM-summber-green',
+      [CATEGORY.테마]: 'bg-BUSIM-deep-lavender',
+      [CATEGORY.레포츠]: 'bg-BUSIM-ball-blue',
+      [CATEGORY.핫플]: 'bg-BUSIM-cherry',
+      [CATEGORY.맛집]: 'bg-BUSIM-beaver',
+      [CATEGORY.특별한_맛집]: 'bg-BUSIM-thulian-pink',
     },
   },
   defaultVariants: {
-    category: '관광지',
+    category: CATEGORY.관광지,
   },
 })
 
 const TextVariants = cva('text-center text-xs font-semibold leading-normal', {
   variants: {
     category: {
-      관광지: 'text-white',
-      자연: 'text-white',
-      테마: 'text-white',
-      레포츠: 'text-white',
-      핫플: 'text-white',
-      맛집: 'text-white',
-      특별한_맛집: 'text-white',
-      술집: 'text-white',
+      [CATEGORY.관광지]: 'text-white',
+      [CATEGORY.자연]: 'text-white',
+      [CATEGORY.테마]: 'text-white',
+      [CATEGORY.레포츠]: 'text-white',
+      [CATEGORY.핫플]: 'text-white',
+      [CATEGORY.맛집]: 'text-white',
+      [CATEGORY.특별한_맛집]: 'text-white',
     },
   },
   defaultVariants: {
-    category: '관광지',
+    category: CATEGORY.관광지,
   },
 })
 
 interface TagProps extends VariantProps<typeof tagVariants> {
-  catId: number
+  category: CategoryType
   tagStyle?: string
   textStyle?: string
   title?: string
 }
 
-export function Tag({ catId, tagStyle, title, textStyle }: TagProps) {
-  const category = Object.keys(CATEGORY).find(key => CATEGORY[key as CategoryKey] === catId) as
-    | CategoryKey
-    | undefined
+export function Tag({ category, tagStyle, title, textStyle }: TagProps) {
+  const categoryKey = Object.keys(CATEGORY).find(
+    key => CATEGORY[key as CategoryKey] === category,
+  ) as CategoryKey | undefined
 
-  const displayText = title || category
+  const displayText = title || categoryKey
 
   return (
     <View className={cn(tagVariants({ category }), tagStyle)}>
