aebd2d5910dbeb28cc82a09f1c486ac712db0d82
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-14 04:53:00 +0900

    feat: user default 이미지 에셋 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,4 @@
 export { default as logoWhite } from './logo-white.png'
 export { default as logoBlue } from './logo-blue.png'
 export { default as logoWelcome } from './logo-welcome.png'
+export { default as user } from './user.png'
