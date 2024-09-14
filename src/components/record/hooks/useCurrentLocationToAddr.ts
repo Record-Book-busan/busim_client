a49010511e0e721deeda3a293f1129fe9cfd2291
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-15 07:19:49 +0900

    refactor: 현재 좌표 → 주소 변환하는 로직 훅으로 분리

---
+++ src/components/record/hooks/useCurrentLocationToAddr.ts
@@ -0,0 +1,29 @@
+import { useLocation } from '@/hooks/useLocation'
+import { useLocationToAddr } from '@/services/record'
+import { formatAddress } from '@/utils/format'
+
+/** 현재 위치 주소로 변환하는 훅입니다. */
+export const useCurrentLocationToAddr = () => {
+  const { location, refreshLocation } = useLocation()
+  const { isLoading, isError, refetch } = useLocationToAddr(location.lat, location.lng)
+
+  console.log('location:', location)
+
+  const getCurrentAddress = async () => {
+    await refreshLocation()
+    const { data } = await refetch()
+
+    if (data) {
+      const address = formatAddress(data)
+      return {
+        name: address.fullAddress,
+        lat: location.lat,
+        lng: location.lng,
+      }
+    }
+
+    throw new Error('[ERROR]: 주소를 가져오는데 실패했습니다.')
+  }
+
+  return { getCurrentAddress, isLoading, isError }
+}
