759e2b94f3c7c4209ea85c1bcbffde9057068832
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-15 07:14:56 +0900

    feat: UpdateRecord 스키마 추가

--- src/types/schemas/record.ts
+++ src/types/schemas/record.ts
@@ -4,11 +4,31 @@ export const PostRecordSchema = z.object({
   lat: z.number().min(-90).max(90),
   lng: z.number().min(-180).max(180),
   title: z.string().min(1).max(100),
-  content: z.string().min(1).max(1000),
+  content: z.string().min(1).max(501),
   imageUrl: z.string(),
 })
-
 export type PostRecord = z.infer<typeof PostRecordSchema>
+
+export const UpdateRecordSchema = z.object({
+  markId: z.number(),
+  lat: z.number().min(-90).max(90),
+  lng: z.number().min(-180).max(180),
+  title: z.string().min(1).max(100),
+  content: z.string().min(1).max(501),
+  imageUrl: z.string(),
+})
+export type UpdateRecord = z.infer<typeof UpdateRecordSchema>
+
+export const RecordDetailSchema = z.object({
+  id: z.number(),
+  title: z.string().min(1).max(100),
+  content: z.string().min(1).max(1000),
+  imageUrl: z.string().url(),
+  lat: z.number().min(-90).max(90),
+  lng: z.number().min(-180).max(180),
+  createdAt: z.string().datetime(),
+})
+export type RecordDetail = z.infer<typeof RecordDetailSchema>
 
 export const RecordListSchema = z.object({
   id: z.number(),
@@ -19,5 +39,4 @@ export const RecordListSchema = z.object({
   lat: z.number(),
   lng: z.number(),
 })
-
 export type RecordList = z.infer<typeof RecordListSchema>
