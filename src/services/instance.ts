3504f54bfad95ce5c30e06bcbbfb2b394db7da7a
chw970320 <chw970320@gmail.com>
2024-09-18 08:00:24 +0900

    feat: 공통 error prototype 추가

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -1,26 +1,69 @@
 import ky from 'ky'
+import { Alert } from 'react-native'
 
 const url = `${process.env.API_URL ? process.env.API_URL : ''}`
+
+interface kyExtendProps {
+  prefixUrl: string
+  headers: {
+    Accept: string
+    Authorization?: string
+  }
+}
+
+const kyExtend = ({ prefixUrl, headers }: kyExtendProps) =>
+  ky.extend({
+    prefixUrl,
+    headers,
+    retry: 0,
+    hooks: {
+      beforeRequest: [
+        request => {
+          console.log('Request:', request.url)
+        },
+      ],
+      afterResponse: [
+        (request, options, response) => {
+          if (!response.ok) {
+            Alert.alert(
+              'API 요청이 실패했습니다.',
+              `에러코드: ${response.status} ${response.statusText}`,
+            )
+            console.error('Response Error:', response.statusText)
+          }
+        },
+      ],
+      beforeError: [
+        error => {
+          Alert.alert(
+            'API 요청이 실패했습니다.',
+            `에러코드 ${error?.response.status || 'Unknown'}: ${error?.response.statusText || 'Unknown error'}`,
+          )
+          console.error(error)
+
+          return error
+        },
+      ],
+    },
+  })
 
 export const instance = (path: string) => {
   const prefixUrl = url + path
 
-  return ky.extend({
-    prefixUrl,
+  return kyExtend({
+    prefixUrl: prefixUrl,
     headers: {
       Accept: 'application/json',
     },
-    retry: 0,
   })
 }
 
 export const kakaoMap = () => {
-  return ky.extend({
+  return kyExtend({
     prefixUrl: 'https://dapi.kakao.com/v2/local/',
     headers: {
       Accept: 'application/json',
       Authorization: `KakaoAK ${process.env.KakaoRestApiKey}`,
     },
-    retry: 0,
   })
 }
