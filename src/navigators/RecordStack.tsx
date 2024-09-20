2b008438ab83bd9e5de00085f4c5b93eb406a1e4
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-20 16:32:33 +0900

    refactor: initialRouteName 추가

--- src/navigators/RecordStack.tsx
+++ src/navigators/RecordStack.tsx
@@ -14,6 +14,7 @@ const Stack = createStackNavigator<RecordStackParamList>()
 function RecordStackNavigator() {
   return (
     <Stack.Navigator
+      initialRouteName="RecordMain"
       screenOptions={{
         headerShown: false,
       }}
