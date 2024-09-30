a9600b7802615c75e5ac85852118e4e99fc547fb
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-30 19:36:08 +0900

    feat: RecordMapView 웹뷰와 통신 로직 추가

--- src/components/record/Place.tsx
+++ src/components/record/Place.tsx
@@ -1,96 +1,13 @@
-import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
-import { useNavigation } from '@react-navigation/native'
-import { type StackNavigationProp } from '@react-navigation/stack'
-import { useState } from 'react'
-import { Alert, TouchableOpacity, View } from 'react-native'
+import { View } from 'react-native'
 
-import { useLocation } from '@/hooks/useLocation'
-import { useNavigateWithPermissionCheck } from '@/hooks/useNavigationPermissionCheck'
-import { SvgIcon, Typo } from '@/shared'
-import { RootStackParamList } from '@/types/navigation'
-
-import { MapFAB, RecordMapView } from '../map'
+import { RecordMapView } from '../map'
 
 export function Place() {
-  const navigation = useNavigation<StackNavigationProp<RootStackParamList, 'CreateRecordStack'>>()
-  const { navigateWithPermissionCheck } = useNavigateWithPermissionCheck()
-  const { location, myPositionValid, refreshLocation } = useLocation()
-  // const [isBookMarkPressed, setIsBookMarkPressed] = useState(false)
-  const [isLocationPressed, setIsLocationPressed] = useState(false)
-
-  const handleClickFindWay = () => {
-    navigateWithPermissionCheck({
-      navigation,
-      routeName: 'SearchStack',
-      params: { screen: 'FindWay' },
-    })
-  }
-
-  const handleLocationPress = () => {
-    void refreshLocation()
-
-    if (myPositionValid) {
-      setIsLocationPressed(prev => !prev)
-    } else {
-      Alert.alert('서비스 제공 위치가 아닙니다', '부산 외 지역은 서비스 제공 지역이 아닙니다.', [
-        { text: '확인', style: 'default' },
-      ])
-    }
-  }
-
-  const bottomTabBarHeight = useBottomTabBarHeight()
-
   return (
-    <View className="mt-1 flex-1">
-      <View
-        className="absolute bottom-6 right-4 z-[2] flex gap-4"
-        style={{
-          paddingBottom: bottomTabBarHeight,
-        }}
-      >
-        {/* 길찾기 표시 버튼 */}
-        <MapFAB onPress={handleClickFindWay} iconName="findWay" />
-        {/* 북마크 표시 버튼 */}
-        {/* <MapFAB
-          onPress={() => setIsBookMarkPressed(!isBookMarkPressed)}
-          iconName="bookmark"
-          enabled={isBookMarkPressed}
-        /> */}
-        {/* 내 위치 버튼 */}
-        <MapFAB onPress={handleLocationPress} iconName="position" enabled={isLocationPressed} />
-      </View>
-
-      <View className={`absolute bottom-24 left-4 z-[2px] flex w-3/4 gap-4`}>
-        <TouchableOpacity
-          style={{
-            shadowColor: '#000',
-            shadowOffset: { width: 0, height: 2 },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            elevation: 5,
-          }}
-          className="flex h-14 items-start justify-center rounded-xl bg-white px-4"
-          onPress={() => {
-            navigateWithPermissionCheck({
-              navigation,
-              routeName: 'CreateRecordStack',
-              params: {
-                screen: 'CreateRecord',
-              },
-            })
-          }}
-        >
-          <View>
-            <Typo className="text-base font-bold text-black">여행 기록 작성하기</Typo>
-            <Typo className="text-xs text-[#ECA39D]">현위치로 기록이 남겨집니다.</Typo>
-          </View>
-          <SvgIcon name="doubleChevronRight" className="absolute right-2 text-black" />
-        </TouchableOpacity>
-      </View>
-
+    <View className="flex-1">
       {/* 지도 웹뷰 */}
       <View className="flex-1">
-        <RecordMapView location={location} isLocationPressed={isLocationPressed} />
+        <RecordMapView />
       </View>
     </View>
   )
