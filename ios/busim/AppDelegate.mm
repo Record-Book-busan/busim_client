a5982045d84e0dbbc1cf1a9a30061b9deb300e6d
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-30 22:16:37 +0900

    build: kakaomap info.plist

--- ios/busim/AppDelegate.mm
+++ ios/busim/AppDelegate.mm
@@ -4,8 +4,6 @@
 #import <React/RCTLinkingManager.h>
 #import <RNCKakaoUser/RNCKakaoUserUtil.h>
 
-#import <KakaoOpenSDK/KakaoOpenSDK.h>
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -14,8 +12,6 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
-  [KOSession sharedSession].appKey = @"572d2540a7893d0c189b2b872193effb";
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
@@ -41,12 +37,6 @@
     return [RNCKakaoUserUtil handleOpenUrl:url];
   }
 
-  // 카카오 세션 처리
-  if ([[KOSession sharedSession] handleOpenURL:url]) {
-    return YES;
-  }
-
-  // React Native Linking 처리
   return [super application:application openURL:url options:options] || [RCTLinkingManager application:application openURL:url options:options];
 }
 
