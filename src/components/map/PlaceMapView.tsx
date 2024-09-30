62e5e380db2020150a4cd0f934e62e5af1b569ed
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-09-30 19:35:22 +0900

    feat: PlaceMapView 웹뷰와 통신 로직 추가

--- src/components/map/PlaceMapView.tsx
+++ src/components/map/PlaceMapView.tsx
@@ -1,33 +1,22 @@
 import { type BottomTabNavigationProp, useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 import { useNavigation } from '@react-navigation/native'
-import { useEffect, useCallback, useMemo, useRef, useState } from 'react'
+import { useEffect, useCallback, useRef, useState, useMemo } from 'react'
 import { View } from 'react-native'
-import { WebView, type WebViewMessageEvent } from 'react-native-webview'
 
+import { useLocation } from '@/hooks/useLocation'
 import { useNavigateWithPermissionCheck } from '@/hooks/useNavigationPermissionCheck'
 import map from '@/services/map'
 import { type PlaceType, useMapPlace, useParking, useToilet } from '@/services/place'
 
 import { RefreshButton } from './RefreshButton'
+import { WebView, WebViewBridge, WebViewEl } from '../common'
+import { MapFAB } from './MapFAB'
 
 import type { RootStackParamList } from '@/types/navigation'
-
-type ReturnProps = {
-  type: string
-  data: {
-    zoomLevel: string
-    lat: number
-    lng: number
-    type: string
-    id: number
-  }
-}
 
 type MapViewProps = {
   activeCategory: string[]
   eyeState: boolean
-  location: { lng: number; lat: number }
-  isLocationPressed: boolean
   isToiletPressed: boolean
   isParkingPressed: boolean
 }
@@ -35,17 +24,22 @@ type MapViewProps = {
 export const PlaceMapView = ({
   activeCategory,
   eyeState,
-  location,
-  isLocationPressed,
   isToiletPressed,
   isParkingPressed,
 }: MapViewProps) => {
-  const webViewRef = useRef<WebView>(null)
+  const webViewRef = useRef<WebViewEl>(null)
+  const webViewBridge = useRef(new WebViewBridge(webViewRef)).current
+
+  const { location, refreshLocation } = useLocation()
+  const [currentLocation, setCurrentLocation] = useState(location)
   const [mapCenter, setMapCenter] = useState(location)
   const [queryCenter, setQueryCenter] = useState(location)
   const [zoomLevel, setZoomLevel] = useState('LEVEL_3')
   const [queryZoomLevel, setQueryZoomLevel] = useState('LEVEL_3')
-  const [needsRefresh, setNeedsRefresh] = useState(false)
+
+  const [isChangedMap, setIsChangedMap] = useState(false)
+  const [isMyLocationActive, setIsMyLocationActive] = useState(false)
+
   const navigation = useNavigation<BottomTabNavigationProp<RootStackParamList, 'MainTab'>>()
   const { navigateWithPermissionCheck } = useNavigateWithPermissionCheck()
   const bottomTabBarHeight = useBottomTabBarHeight()
@@ -81,12 +75,50 @@ export const PlaceMapView = ({
   )
 
   useEffect(() => {
-    if (!webViewRef.current) return
+    webViewBridge.onEvent('ZOOM_CHANGE', data => {
+      if (data.zoomLevel) {
+        setZoomLevel(data.zoomLevel)
+        setIsChangedMap(true)
+      }
+    })
+
+    webViewBridge.onEvent('POSITION_CHANGE', data => {
+      if (data.lat && data.lng) {
+        setMapCenter({ lat: data.lat, lng: data.lng })
+        setIsChangedMap(true)
+      }
+    })
+
+    webViewBridge.onEvent('OVERLAY_CLICK', data => {
+      if (data.id && data.type) {
+        navigateWithPermissionCheck({
+          navigation,
+          routeName: 'MapStack',
+          params: {
+            screen: 'MapDetail',
+            params: {
+              id: data.id,
+              type: data.type as PlaceType,
+            },
+          },
+        })
+      }
+    })
+
+    return () => {
+      webViewBridge.offEvent('ZOOM_CHANGE')
+      webViewBridge.offEvent('POSITION_CHANGE')
+      webViewBridge.offEvent('OVERLAY_CLICK')
+    }
+  }, [])
+
+  const updateOverlays = useCallback(async () => {
+    if (!webViewBridge) return
 
     if (eyeState) {
       const data = [
         ...(placeData || []).map(r => ({
-          title: r.id.toString(),
+          id: r.id.toString(),
           category: r.category,
           type: r.type,
           lat: r.lat,
@@ -94,7 +126,7 @@ export const PlaceMapView = ({
         })),
         ...(isToiletPressed && toiletData
           ? toiletData.map(r => ({
-              title: r.toiletName,
+              id: r.toiletName,
               category: 'TOILET',
               lat: r.latitude,
               lng: r.longitude,
@@ -102,88 +134,80 @@ export const PlaceMapView = ({
           : []),
         ...(isParkingPressed && parkingData
           ? parkingData.map(r => ({
-              title: r.id.toString(),
+              id: r.id.toString(),
               category: 'PARKING',
               lat: r.lat,
               lng: r.lng,
             }))
           : []),
       ]
-      webViewRef.current.injectJavaScript(
-        `settingPlaceOverlays(${JSON.stringify(activeCategory)}, ${JSON.stringify(data)})`,
-      )
+
+      try {
+        await webViewBridge.sendRequest('GET_PLACE_DATA', data)
+      } catch (error) {
+        console.error('[ERROR] 오버레이 업데이트 실패:', error)
+      }
     } else {
-      // eyeState가 false일 때 오버레이 제거
-      webViewRef.current.injectJavaScript('removeOverlays();')
+      try {
+        await webViewBridge.sendRequest('GET_OVERLAY_STATE', eyeState)
+      } catch (error) {
+        console.error('[ERROR] 오버레이 제거 실패:', error)
+      }
     }
-  }, [
-    eyeState,
-    zoomLevel,
-    mapCenter,
-    placeData,
-    toiletData,
-    parkingData,
-    activeCategory,
-    isToiletPressed,
-    isParkingPressed,
-  ])
+  }, [eyeState, placeData, toiletData, parkingData, isToiletPressed, isParkingPressed])
 
   useEffect(() => {
-    setQueryCenter(mapCenter)
-    setQueryZoomLevel(zoomLevel)
-    setNeedsRefresh(false)
+    void updateOverlays()
+  }, [updateOverlays])
+
+  useEffect(() => {
+    // 활성화된 카테고리가 바뀌거나, MapFAB들을 클릭했을 때 isChangedMap 상태 초기화
+    setIsChangedMap(false)
   }, [activeCategory, isToiletPressed, isParkingPressed])
 
   useEffect(() => {
-    if (webViewRef.current) {
-      webViewRef.current.injectJavaScript(`moveMap(${JSON.stringify(location)})`)
+    // 지도 중심이 변경될 때마다 현재 위치와 비교
+    const isSameLocation =
+      Math.abs(mapCenter.lat - currentLocation.lat) < 0.0001 &&
+      Math.abs(mapCenter.lng - currentLocation.lng) < 0.0001
+
+    console.log('isSameLocation', mapCenter.lat, currentLocation.lat)
+    console.log('isSameLocation', mapCenter.lng, currentLocation.lng)
+
+    setIsMyLocationActive(isSameLocation)
+  }, [mapCenter, currentLocation])
+
+  const handleLocationPress = async () => {
+    const coords = await refreshLocation()
+    if (coords) {
+      try {
+        const response = await webViewBridge.sendRequest('GET_CURRENT_LOCATION', coords)
+        setCurrentLocation(response.payload)
+        setIsMyLocationActive(true)
+      } catch (error) {
+        console.error('[ERROR] 현재 위치 전송 실패:', error)
+      }
     }
-  }, [isLocationPressed, location])
+  }
 
   const handleRefresh = useCallback(() => {
     setQueryCenter(mapCenter)
     setQueryZoomLevel(zoomLevel)
-    setNeedsRefresh(false)
-  }, [mapCenter, zoomLevel])
-
-  const handleMessage = (event: WebViewMessageEvent) => {
-    const eventData: ReturnProps = JSON.parse(event.nativeEvent.data)
-
-    switch (eventData.type) {
-      case 'zoomChanged':
-        setZoomLevel(eventData.data.zoomLevel)
-        setNeedsRefresh(true)
-        break
-      case 'dragend':
-        setMapCenter({ lat: eventData.data.lat, lng: eventData.data.lng })
-        setNeedsRefresh(true)
-        break
-      case 'overlayClick':
-        console.log(eventData.data.type)
-        navigateWithPermissionCheck({
-          navigation,
-          routeName: 'MapStack',
-          params: {
-            screen: 'MapDetail',
-            params: {
-              id: eventData.data.id,
-              type: eventData.data.type as PlaceType,
-            },
-          },
-        })
-        break
-    }
-  }
-
-  const isRefreshNeeded =
-    needsRefresh &&
-    activeCategory.length > 0 &&
-    (mapCenter.lat !== queryCenter.lat ||
-      mapCenter.lng !== queryCenter.lng ||
-      zoomLevel !== queryZoomLevel)
+    setIsChangedMap(false)
+  }, [mapCenter])
 
   return (
     <>
+      {/* 내 위치 버튼 */}
+      <View
+        className="absolute bottom-10 right-4 z-[2]"
+        style={{
+          paddingBottom: bottomTabBarHeight,
+        }}
+      >
+        <MapFAB onPress={handleLocationPress} iconName="position" enabled={isMyLocationActive} />
+      </View>
+      {/* 지도 웹뷰 */}
       <WebView
         ref={webViewRef}
         source={{ html: map }}
@@ -192,11 +216,13 @@ export const PlaceMapView = ({
             const map = createMap(${JSON.stringify(location)})
           })
         `}
-        onMessage={handleMessage}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
+        onMessage={webViewBridge.handleMessage}
+        javaScriptEnabled
+        domStorageEnabled
+        debug={true}
       />
-      {isRefreshNeeded && (
+      {/* 현재 위치에서 재검색 버튼 */}
+      {isChangedMap && activeCategory.length > 0 && (
         <View className="flex-row items-center justify-center">
           <View
             className="absolute bottom-10 z-[2]"
