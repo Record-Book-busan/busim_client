6e20bfea1dc6338e1e3483b8c9a55f2df4663d7c
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-10-01 22:50:07 +0900

    fix: 바뀐 인터페이스에 맞게 수정

--- src/screens/MyPage/RecordList.tsx
+++ src/screens/MyPage/RecordList.tsx
@@ -1,18 +1,22 @@
 import { type CompositeNavigationProp, useNavigation } from '@react-navigation/native'
-import { ActivityIndicator, FlatList, Text, View } from 'react-native'
+import { ActivityIndicator, FlatList, View } from 'react-native'
 
 import { ImagePlaceItem } from '@/components/search'
-import { CategoryType, getCategoryText } from '@/constants'
 import { useNavigateWithPermissionCheck } from '@/hooks/useNavigationPermissionCheck'
 import { useInfiniteRecordList } from '@/services/record'
+import { Typo } from '@/shared'
 import { type RecordList } from '@/types/schemas/record'
 
-import type { MyPageStackParamList, RootStackParamList } from '@/types/navigation'
+import type { MainTabParamList, MyPageStackParamList, RootStackParamList } from '@/types/navigation'
+import type { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
 import type { StackNavigationProp } from '@react-navigation/stack'
 
 type NavigationProps = CompositeNavigationProp<
   StackNavigationProp<MyPageStackParamList, 'RecordList'>,
-  StackNavigationProp<RootStackParamList>
+  CompositeNavigationProp<
+    BottomTabNavigationProp<MainTabParamList, 'Record'>,
+    StackNavigationProp<RootStackParamList>
+  >
 >
 
 export default function RecordListScreen() {
@@ -31,45 +35,42 @@ export default function RecordListScreen() {
           screen: 'ReadRecord',
           params: { id },
         },
+        initial: false,
       },
     })
-  }
-
-  const handleBookmarkPress = (id: number) => {
-    console.log(id)
   }
 
   const renderItem = ({ item }: { item: RecordList }) => (
     <ImagePlaceItem
       id={item.id}
       title={item.title}
-      category={getCategoryText(item.touristCategory as CategoryType)}
       content={item?.content}
       onPressMove={() => handleItemPress(item.id)}
-      imageUrl={item.thumbnailUrl || ''}
-      isBookMarked={false}
-      onPressBookMark={handleBookmarkPress}
+      imageUrl={item.imageUrl || ''}
     />
   )
 
   return (
     <View className="flex-1 bg-white">
-      <View className="px-4">
-        <FlatList
-          data={data.pages.flatMap(page => page)}
-          renderItem={renderItem}
-          keyExtractor={item => item.id.toString()}
-          onEndReached={() => hasNextPage && fetchNextPage()}
-          onEndReachedThreshold={0.5}
-          ListEmptyComponent={<Text className="p-4">작성한 기록이 없습니다.</Text>}
-          ListFooterComponent={isFetchingNextPage ? <ActivityIndicator /> : null}
-          contentContainerStyle={{
-            flexGrow: 1,
-          }}
-          showsVerticalScrollIndicator={false}
-          showsHorizontalScrollIndicator={false}
-        />
-      </View>
+      <FlatList
+        data={data.pages.flatMap(page => page)}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        onEndReached={() => hasNextPage && fetchNextPage()}
+        onEndReachedThreshold={0.5}
+        ListEmptyComponent={
+          <View className="flex-1 items-center justify-center">
+            <Typo className="p-4 text-gray-700">작성한 기록이 없습니다.</Typo>
+          </View>
+        }
+        ListFooterComponent={isFetchingNextPage ? <ActivityIndicator /> : null}
+        contentContainerStyle={{
+          flexGrow: 1,
+          paddingHorizontal: 16,
+        }}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   )
 }
